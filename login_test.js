66bd35ecc68062b09b6f297fb9fce98f73925633
Landeivison <lanrhema@gmail.com>
2024-04-08 17:55:03 -0300

    login_teste

---
+++ login_test.js
@@ -0,0 +1,41 @@
+Feature('login');
+
+Scenario('Login com sucesso',  ({ I }) => {
+
+    I.amOnPage('http://automationpratice.com.br/')
+    I.click('Login')
+    I.fillField('#user', 'lan@gmail.com')
+    I.fillField('#password', secret('123456'))
+    I.click('#btnLogin')
+    I.waitForText('Login realizado', 3)
+    
+});
+
+Scenario('Tentando Logar digitando apenas o e-mail',  ({ I }) => {
+
+    I.amOnPage('http://automationpratice.com.br/')
+
+    
+});
+
+Scenario('Tentando logar sem digitar e-mail e senha',  ({ I }) => {
+
+    I.amOnPage('http://automationpratice.com.br/')
+
+    
+});
+
+Scenario('Tentando Logar digitando apenas a senha',  ({ I }) => {
+
+    I.amOnPage('http://automationpratice.com.br/')
+
+    
+});
+
+Scenario('Tentando Logar sem preencher os campos e-mail e senha',  ({ I }) => {
+
+    I.amOnPage('http://automationpratice.com.br/')
+
+    
+});
+
